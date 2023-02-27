7cf005611a226bc9fdc76224c7dd9bb4d5c4c208
Darshil Mehta <darshilm223@gmail.com>
2023-02-27 15:56:16 +0530

    feat: update predict API

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
 import tensorflow as tf
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
+from math import exp
 
 app = FastAPI()
 
@@ -14,26 +15,43 @@ app.add_middleware(
   allow_headers=["*"],
 )
 
-model = tf.keras.models.load_model('./models/ankle_best_run.h5')
+ankle_one = tf.keras.models.load_model('./models/ankle_best_run.h5')
+
+strToModel = {
+  "ankle_one": ankle_one
+}
 
 @app.post("/predict")
-async def predict(file: UploadFile = File(...)):
-  test_data = load_image_into_numpy_array(await file.read())
-  result = model.predict(test_data)
-  result = result.reshape(4,)
-  result = result * 100
-  total_sum = sum(result)
-  label = np.argmax(result)
-  confidence = float(result[label].round(2))/total_sum
-  return {
-    "result": str(label),
-    "confidence": str(confidence)
-  }
+async def predict(modelName: str = Form(...), file: UploadFile = File(...)):
+  try:
+    model = strToModel[modelName]
+    test_data = load_image_into_numpy_array(await file.read())
+    result = model.predict(test_data)
+    result = result.reshape(4,)
+    label = np.argmax(result)
+    confidence = softmax(result.tolist(), label)
+    return {
+      "result": str(label),
+      "confidence": str(confidence)
+    }
+  except KeyError as k:
+    return {
+      "error": True,
+      "message": "Model not found, please try again later!",
+      "modelName": k
+    }
+
+def softmax(vector: list, label: int):
+  e = []
+  print(vector)
+  for i in vector:
+    e.append(exp(i))
+  return e[label] / sum(e)
 
 def load_image_into_numpy_array(data):
-    npimg = np.frombuffer(data, np.uint8)
-    frame = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-    cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    image = cv2.resize(frame, (224, 224))
-    test_data = np.array(image).reshape(1, 224, 224, 3)
-    return test_data
+  npimg = np.frombuffer(data, np.uint8)
+  frame = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+  cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+  image = cv2.resize(frame, (224, 224))
+  test_data = np.array(image).reshape(1, 224, 224, 3)
+  return test_data
